e1c691e22f000ffd43d57c5914dd4b92f0fecc95
flonitoo <73523574+flonitoo@users.noreply.github.com>
2025-11-27 18:33:05 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,11 @@
+output "load_balancer_dns" {
+  value = aws_lb.web_lb.dns_name
+}
 
+output "db_endpoint" {
+  value = aws_db_instance.app_db.endpoint
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
+}
