d276a2282ec863b3c2b07c439faf18b43be807d4
flonitoo <73523574+flonitoo@users.noreply.github.com>
2025-11-27 18:32:07 +0100

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,1 +1,18 @@
+resource "aws_db_subnet_group" "db_subnets" {
+  name       = "db-subnets"
+  subnet_ids = [aws_subnet.public.id]
+}
 
+resource "aws_db_instance" "app_db" {
+  identifier         = "app-db"
+  engine             = "mysql"
+  instance_class     = "db.t3.micro"
+  allocated_storage  = 20
+  name               = "appdb"
+  username           = var.db_username
+  password           = var.db_password
+  skip_final_snapshot = true
+
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.db_subnets.name
+}
