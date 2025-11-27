c986c0c5af1b5d1e90bbc659f2e34e2b51910bbb
flonitoo <73523574+flonitoo@users.noreply.github.com>
2025-11-27 18:29:36 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,15 @@
+variable "region" {
+  default = "eu-central-1"
+}
 
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "db_username" {
+  default = "admin"
+}
+
+variable "db_password" {
+  default = "password123"
+}
