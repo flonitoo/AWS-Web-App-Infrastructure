631adc0a3c2cd5655a1fe62ff8ab735b45b37a82
flonitoo <73523574+flonitoo@users.noreply.github.com>
2025-11-27 18:31:22 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,28 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+
+  owners = ["137112412989"]
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.public.id
+  security_groups = [aws_security_group.ec2_sg.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum install -y nginx
+    systemctl enable nginx
+    systemctl start nginx
+  EOF
+
+  tags = {
+    Name = "web-server"
+  }
+}
